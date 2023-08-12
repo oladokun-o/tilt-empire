f632657cbd9b323ef87a84016f1b0fdbf6660ee7
Oladokun Oladipupo <oladipupooladokun@gmail.com>
2023-08-12 14:57:49 +0100

    video update

---
+++ src/app/shared/components/video/video.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-video',
+  templateUrl: './video.component.html',
+  styleUrls: ['./video.component.css']
+})
+export class VideoComponent implements OnInit {
+
+  video!: string;
+  volume: number = 0.2;
+  controls: boolean = false;
+  loop: boolean = true;
+
+  constructor (
+    public activeModal: NgbActiveModal
+  ) { }
+
+  ngOnInit() {
+    console.log(this.video);
+  }
+
+}
